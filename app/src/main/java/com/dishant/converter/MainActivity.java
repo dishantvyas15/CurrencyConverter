db0fe7c83271f44a508401aa9603b2fbf83c5e77
unknown <dishantvyas15@gmail.com>
2020-09-27 15:13:40 +0530

    First commit to the repo

---
+++ app/src/main/java/com/dishant/converter/MainActivity.java
@@ -0,0 +1,73 @@
+package com.dishant.converter;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert_inr_to_usd(View view) {
+        EditText inrAmount = findViewById(R.id.inrAmount);
+        String rupees = inrAmount.getText().toString();
+        Double inr = Double.parseDouble(rupees);
+        Double usd = inr/71.24;
+        Toast.makeText(this, "$ " + usd.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void convert_usd_to_inr(View view) {
+        EditText usdAmount = findViewById(R.id.usdAmount);
+        String dollars = usdAmount.getText().toString();
+        Double usd = Double.parseDouble(dollars);
+        Double inr = usd*71.24;
+        Toast.makeText(this, "Rs. " + inr.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Feedback sent to app developer.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
